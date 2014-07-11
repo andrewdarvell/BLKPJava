3a81287481504caa1504817b5224e1fe1fe2bd34
andrewdarvell <andrew@darvell.ru>
2014-07-11 17:39:32 +0400

    first commit

---
+++ src/ru/darvell/blkp/Server.java
@@ -0,0 +1,32 @@
+package ru.darvell.blkp;
+
+import java.net.ServerSocket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: darvell
+ * Date: 11.07.14
+ * Time: 13:39
+ * LastFM plugin for BLKPlayer.
+ */
+
+public class Server {
+	static int port = 3425;
+	public static void main(String args[]){
+
+		try{
+			ServerSocket serverSocket = new ServerSocket(port);
+			System.out.println("server is started");
+			Heap heap = new Heap();
+			new Worker(heap);
+
+
+
+			while(true){
+				new SocketWorker(serverSocket.accept(), heap);
+			}
+		}catch (Exception e){
+			System.out.println("Error "+e.getMessage());
+		}
+	}
+}
---
+++ src/ru/darvell/blkp/Worker.java
@@ -0,0 +1,38 @@
+package ru.darvell.blkp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: darvell
+ * Date: 11.07.14
+ * Time: 17:10
+ * Class execute commands
+ */
+public class Worker extends Thread{
+
+	Heap heap;
+	Thread t;
+
+	Worker(Heap heap){
+		System.out.println("worker create");
+		this.heap = heap;
+		t = this;
+		t.start();
+	}
+
+	@Override
+	public void run() {
+		try{
+			while(true){
+
+				if(heap.getCount()>0){
+					String command = heap.getCommand();
+					System.out.println("Worker: "+command);
+				}
+				t.sleep(500);
+			}
+
+		}catch (Exception e){
+			System.out.println("Error: "+e.getMessage());
+		}
+	}
+}
